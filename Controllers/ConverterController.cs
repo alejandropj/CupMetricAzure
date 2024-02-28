7c0e7570d20a59ff6b515d8d7b6d074d7afab788
alejandropj <alejandroparrajimenez@gmail.com>
2024-02-28 14:13:22 +0100

    Add project files.

---
+++ Controllers/ConverterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CupMetric.Controllers
+{
+    public class ConverterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Ingrediente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CupMetric.Models
+{
+    [Table("INGREDIENTE")]
+    public class Ingrediente
+    {
+        [Key]
+        [Column("IdIngrediente")]
+        public int IdIngrediente { get; set; }        
+        [Column("Nombre")]
+        public string Nombre { get; set; }
+    }
+}
