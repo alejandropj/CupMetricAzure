7d23cdecab0bc6f197dfa395fb86bd9f63a55daf
alejandropj <alejandroparrajimenez@gmail.com>
2024-02-29 13:53:17 +0100

    Some fixes in Design

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using CupMetric.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CupMetric.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            return View();
+        }        
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User usuario)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CupMetric.Models
+{
+    public class User
+    {
+        [Key]
+        [Column("IDUSUARIO")]
+        public int IdUsuario { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("EMAIL")]
+        public string Email { get; set; }
+        [Column("PASSWORD")]
+        public string Password { get; set; }
+    }
+}
